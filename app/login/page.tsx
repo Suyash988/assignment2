7bbb0922bd757f0d3c7da809ff1f31f5af5a5795
SUYASH AGNIHOTRI <suyashagnihotri988@gmail.com>
2024-06-22 16:56:22 +0530

    second commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,30 +1,65 @@
+'use client'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
+import axios from 'axios'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const page = () => {
-  return (
     
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('');
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    
+     const handleLogin = async (e: React.FormEvent) => {
+      e.preventDefault(); 
+      try { 
+           const url = 'http://localhost:3030/login/user'
+           const response = await axios.post((url), {email, password})
+           alert(response.data.message)
+        } catch (error) {
+          console.log(error)
+          alert('Error logging in User')
+        } finally {
+          setIsLoading(false)
+        }
+      }
+    
+  return ( 
     <div className='w-full text-center mt-4'>
-    <h1 className="">Login</h1>
-    <form action="" className="">
-    <div className="flex flex-col gap-4 p-4">
-    <TextField id="outlined-basic" label="Email" variant="outlined" />
-    <TextField id="outlined-basic" label="Password" variant="outlined" type="password" />
-    </div>
-    <div>
-  <Link href="/">
-    <Button variant="contained">Login</Button>
-  </Link>  
-    </div>
+       <h1 className="">Login</h1>
+         <form onSubmit={handleLogin}>
+           <div className="flex flex-col gap-4 p-4">
+              <TextField 
+                id="outlined-basic" 
+                label="Email" 
+                variant="outlined" 
+                value={email} 
+                type='email' 
+                onChange={(e) => setEmail(e.target.value)} />
+              <TextField 
+                id="outlined-basic" 
+                label="Password" 
+                variant="outlined" 
+                value={password} 
+                type="password" 
+                onChange={(e) => setPassword(e.target.value)} />
+           </div>
+          <div className='rounded-sm'>
+            <Button 
+            variant="contained" 
+            type='submit'
+            disabled= {isLoading}
+            >
+              { isLoading ? "Signing...": "Login"}
+            </Button>
+          </div>
+        </form>  
     <div className="flex text-center items-center justify-center p-4">
         <p>Don't have an account?</p>
-        <Link href="/signup" className='hover:outline '>Register</Link>
+        <Link href="/signup" className='hover:underline hover:text-blue-700 '>Register</Link>
     </div>
-  </form>
-
 </div>
   )
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,38 +1,88 @@
 'use client'
-import { signup } from '@/utils/action'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
+import axios from 'axios'
 import Link from 'next/link'
 import React, { useState } from 'react'
 
 const page = () => {
 
-  const [data, isData] = useState();
-//   const useEffect(()) => {
-//     effect
-//     return () => {
-//         cleanup
-//     }
-//   }, [input]) 
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const handleSignUp = async (e: React.FormEvent) => {
+      e.preventDefault();
+       try {
+        const url = 'http://localhost:3030/register/user'
+        const response = await axios.post((url), {firstName, lastName, email, password}) 
+        alert(response.data.message)
+       } catch (error) {
+         console.log(error)
+         alert('Unable to register the user')
+       } finally {
+        setIsLoading(false)
+       }
+  }
+
   return (
-    <div className='w-full text-center mt-4'>
-        <form >
-        <h1 className="">Sign Up</h1>
+    <div className='w-full items-center justify-center align-middle text-center mt-4'>
+      <h1 className="">Sign Up</h1>  
+       <form onSubmit={handleSignUp} >
         <div className="flex flex-col gap-4 p-4">
-        <TextField id="outlined-basic" label="First Name" variant="outlined" />
-        <TextField id="outlined-basic" label="Last Name" variant="outlined" />
-        <TextField id="outlined-basic" label="Email" variant="outlined" />
-        <TextField id="outlined-basic" label="Password" variant="outlined" type='password' />
+        <TextField 
+        id="outlined-basic" 
+        label="First Name" 
+        variant="outlined"
+        type="text"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+        required
+        />
+        <TextField 
+        id="outlined-basic" 
+        label="Last Name" 
+        variant="outlined"
+        type='text'
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+        required
+        />
+        <TextField 
+        id="outlined-basic"
+        label="Email" 
+        variant="outlined"
+        type='email'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        />
+        <TextField 
+        id="outlined-basic" 
+        label="Password" 
+        variant="outlined" 
+        type='password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+        />
         </div>
-        <div>
-      <Link href="/">
-        <Button variant="contained">Register</Button>
-      </Link>  
+        <div className='mt-2'>
+        
+        <Button 
+        variant="contained"
+        type='submit'
+        disabled={isLoading}
+         >
+          {isLoading ? "Registering..." : "Register"}
+        </Button>
         </div>
     </form>
         <div className="flex text-center items-center justify-center p-4">
             <p>Already have an account?</p>
-            <Link href="/login" className='hover:outline '>Login</Link>
+            <Link href="/login" className='hover:text-blue-700 hover:underline '>Login</Link>
         </div>
     </div>
     
